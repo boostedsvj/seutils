eb63f8db806b1bf8d5f94e6db53e415c2ead3926
Thomas Klijnsma <tklijnsm@gmail.com>
2022-11-23 14:41:34 -0600

    fix to nonzeroexitcode exception

--- seutils/__init__.py
+++ seutils/__init__.py
@@ -372,7 +372,7 @@ class RmSafetyTrigger(ExceptionWithPath):
 class NonZeroExitCode(subprocess.CalledProcessError):
     def __init__(self, exitcode='?', cmd='?'):
         self.returncode = exitcode
-        super(Exception, self).__init__('Exit code {} for command {}'.format(cmd, exitcode))
+        super(subprocess.CalledProcessError, self).__init__('Exit code {} for command {}'.format(cmd, exitcode))
 
 
 # _______________________________________________________
